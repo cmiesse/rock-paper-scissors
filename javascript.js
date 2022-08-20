1ac34d157fadf5be60075d21494b97079f22b36c
cmiesse <clement.miesse@gmail.com>
2022-08-20 16:42:29 +0200

    Add function game() - 5 games in a row

--- javascript.js
+++ javascript.js
@@ -5,17 +5,41 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-    const player = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if(!possibilities.includes(player)) return "Wrong item entered";
-    if(player == computerSelection) return `Tie : ${player} - ${computerSelection}`
+    const player = transformInput(playerSelection);
+    if(!possibilities.includes(player)) return undefined;
+    if(player == computerSelection) return 0
     else if((player == "Rock" && computerSelection == "Scissors") || 
     (player == "Paper" && computerSelection == "Rock") ||
     (player == "Scissors" && computerSelection == "Paper")
-    ) return `You Win! ${player} beats ${computerSelection}`
-    else return `You Lose! ${computerSelection} beats ${player}`
+    ) return 1
+    else return -1
+}
+
+function transformInput(input){
+    return input.slice(0,1).toUpperCase() + input.slice(1).toLowerCase();
 }
 
 
-const playerSelection = prompt("Enter Rock, Paper or Scissors : ");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));    
+function game(){
+    let playerWins = 0;
+    let computerWins = 0;
+    for(let i = 0; i<5; i++){
+        console.log("Game "+(i+1));
+        const playerSelection = prompt("Game "+(i+1)+" - Enter Rock, Paper or Scissors : ");
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        if(result == undefined) console.log("You entered a wrong set");
+        else if(result == 0) console.log(`Tie ${transformInput(playerSelection)}-${computerSelection}`);
+        else if(result == -1) {
+            console.log(`You Lose! ${computerSelection} beats ${transformInput(playerSelection)}`);
+            computerWins++;
+        } else{
+            console.log(`You Win! ${transformInput(playerSelection)} beats ${computerSelection}`);
+            playerWins++;
+        }
+    }
+
+    console.log("Final Result : ", playerWins == computerWins ? "Tie" : (playerWins < computerWins ? "You lose" : "You win"));
+}
+
+game()
